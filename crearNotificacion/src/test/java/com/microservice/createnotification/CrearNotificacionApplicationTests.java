ee6f3536f7ed8b87b4f79b000a316bbc98a3d6d3
Emilio J Alarcon <ejalarcon@gmail.com>
2016-10-11 13:56:34 +0200

    se borra test

--- crearNotificacion/src/test/java/com/microservice/createnotification/CrearNotificacionApplicationTests.java
+++ crearNotificacion/src/test/java/com/microservice/createnotification/CrearNotificacionApplicationTests.java
@@ -3,10 +3,6 @@ package com.microservice.createnotification;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.bson.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.microservice.createnotification.pojo.Notification;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 
 
 @RunWith(SpringRunner.class)
@@ -54,32 +46,5 @@ public class CrearNotificacionApplicationTests
 	}
 
 
-	@Test
-	public void crearNotificacionSergio()
-	{
-
-		String idTipo = "47";
-		final String host = "mongodb://w963363D.ad.bbva.com:27017/dbTaller";
-
-		MongoClientURI mongoClientURI = new MongoClientURI(host);
-
-		MongoClient mongoClient = new MongoClient(mongoClientURI);
-
-		MongoDatabase db = mongoClient.getDatabase(mongoClientURI.getDatabase());
-
-		MongoCollection<Document> collection = db.getCollection("notificaciones");
-
-
-		Document document = new Document("Alerta", new Document("Tipo", idTipo).append("Caracteristicas",
-				new Document("Titulo", "Cargo superior tarjeta.").append("Descripcion", "Cargo superior en tarjeta con importe de 1.000€ superior al que tiene configurado 900€.")));
-
-		List<Document> arg0 = new ArrayList<Document>();
-
-		arg0.add(document);
-
-		collection.insertMany(arg0);
-
-		mongoClient.close();
-	}
 
 }
