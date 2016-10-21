26515811a74ca482153442b5979bd5934b5bb52c
MORA GARCIA <XE31089@ad.bbva.com>
2016-10-21 14:57:31 +0200

    Añadir nuevas anotaciones a eureka.

--- eurekaserver/src/main/java/com/eureka/server/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/eureka/server/EurekaserverApplication.java
@@ -1,9 +1,14 @@
 package com.eureka.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableEurekaServer
+@EnableAutoConfiguration
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
