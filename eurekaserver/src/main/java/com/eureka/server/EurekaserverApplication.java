019e050593790898b8b1fd334f0476e49df122c2
MORA GARCIA <XE31089@ad.bbva.com>
2016-10-24 15:14:42 +0200

    Configuración eurekaserver.

--- eurekaserver/src/main/java/com/eureka/server/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/eureka/server/EurekaserverApplication.java
@@ -2,7 +2,6 @@ package com.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 
