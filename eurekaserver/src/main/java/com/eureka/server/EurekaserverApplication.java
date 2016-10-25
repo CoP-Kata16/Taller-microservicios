d25331e711b33c38575a4d8073af8351a23bb7bd
Emilio J Alarcon <ejalarcon@gmail.com>
2016-10-25 13:55:18 +0200

    Ajustes librerias eureka

--- eurekaserver/src/main/java/com/eureka/server/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/eureka/server/EurekaserverApplication.java
@@ -1,13 +1,15 @@
 package com.eureka.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
+//@EnableEurekaServer
+//@EnableAutoConfiguration
+
 @EnableEurekaServer
-@EnableAutoConfiguration
+@SpringBootApplication
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
