86b5dd5cf52ab11e16c23c8b77128d6f8741972a
MORA GARCIA <XE31089@ad.bbva.com>
2016-10-26 13:02:20 +0200

    Añado interfaz para crearNotificacion en ribbon.

---
+++ ribbonbalancer/src/main/java/com/ribbon/CrearNotificacionController.java
@@ -0,0 +1,61 @@
+package com.ribbon;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.createnotification.pojo.Notification;
+
+
+@RestController
+@RequestMapping("/ribbon")
+public class CrearNotificacionController 
+{
+
+	@Autowired
+	private ICrearNotificacion notificacionController;
+	
+	
+	
+	@RequestMapping(value = "/getAll", method = RequestMethod.POST)
+	public List<Notification> getAllNotifications()
+	{
+		return notificacionController.getAllNotifications();
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public Notification create(@RequestBody Notification notification)
+	{
+		return notificacionController.insert(notification);
+	}
+	
+	@RequestMapping(value = "/deleteAll", method = RequestMethod.POST)
+	public void deleteAll()
+	{
+		notificacionController.deleteAll();
+	}
+
+	@RequestMapping(value = "/deleteById", method = RequestMethod.POST)
+	public void deleteById(@RequestParam(value = "id") String id)
+	{
+		notificacionController.delete(id);
+	}
+
+	@RequestMapping(value = "/getById", method = RequestMethod.POST)
+	public Notification getById(@RequestParam(value = "id") String id)
+	{
+		return notificacionController.findOne(id);
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public Notification save(@RequestBody Notification notification)
+	{
+		return notificacionController.save(notification);
+	}
+	
+}
---
+++ ribbonbalancer/src/main/java/com/ribbon/ICrearNotificacion.java
@@ -0,0 +1,35 @@
+package com.ribbon;
+
+import java.util.List;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.microservice.createnotification.pojo.Notification;
+
+@FeignClient("crearnotificacion")
+public interface ICrearNotificacion 
+{
+	
+	@RequestMapping(value = "/notification/findAll", method = RequestMethod.POST)
+	public List<Notification> getAllNotifications();
+	
+	@RequestMapping(value = "/notification/create", method = RequestMethod.POST)
+	public Notification insert(@RequestBody Notification notification);
+	
+	@RequestMapping(value = "/notification/deleteAll", method = RequestMethod.POST)
+	public void deleteAll();
+	
+	@RequestMapping(value = "/notification/deleteById", method = RequestMethod.POST)
+	public void delete(@RequestParam(value = "id") String id);
+	
+	@RequestMapping(value = "/notification/getById", method = RequestMethod.POST)
+	public Notification findOne(@RequestParam(value = "id") String id);
+	
+	@RequestMapping(value = "/notification/update", method = RequestMethod.POST)
+	public Notification save(@RequestBody Notification notification);
+	
+}
